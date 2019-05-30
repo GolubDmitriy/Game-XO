42588bf54d12d1cd8974bb2b297f673f17786293
Dmitriy <golub.d.a@yandex.ru>
2019-05-30 16:30:43 +0300

    Добавлен gameField для мониторинга состояния игры

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const cells = document.getElementsByClassName('cell');
 let player = 'X';
+let gameField = []
+
+for (let i = 0; i < 3; ++i) {
+    gameField.push([])
+}
 
 for (cell of cells) {
     cell.addEventListener('click', gameStep);
 }
 
 function gameStep(event) {
-    if (event.target.innerHTML) {
-        return
-    }
-    if (event.target.innerHTML !== player) {
+    if (!event.target.innerHTML) {
         event.target.innerHTML = player;
-    } 
-    changePlayer();
+        gameFieldAddValue([event.target.dataset.value])
+        changePlayer();
+    }
 }
 
 function changePlayer() {
@@ -21,4 +24,8 @@ function changePlayer() {
     } else {
         player = 'X';
     }
+}
+
+function gameFieldAddValue(value) {
+    gameField[Math.floor(value / 3)][value % 3] = player
 }
