d67fc65617e935eec497f6a8ad78d399bb0c5fa5
Dmitriy <golub.d.a@yandex.ru>
2019-05-30 14:52:25 +0300

    Добавлена смена игрока при клике

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const cells = document.getElementsByClassName('cell');
+let player = 'X';
 
 for (cell of cells) {
-    cell.addEventListener('click', gameStep)
+    cell.addEventListener('click', gameStep);
 }
 
 function gameStep(event) {
-    if (!event.target.innerHTML) {
-        event.target.innerHTML = 'X'
+    if (event.target.innerHTML) {
+        return
+    }
+    if (event.target.innerHTML !== player) {
+        event.target.innerHTML = player;
+    } 
+    changePlayer();
+}
+
+function changePlayer() {
+    if (player === 'X') {
+        player = 'O';
     } else {
-        console.log('no')
+        player = 'X';
     }
 }
