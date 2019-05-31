0ffaa07887b95a3630e19f36d0e67bf1e5095db9
Dmitriy <golub.d.a@yandex.ru>
2019-05-31 11:07:01 +0300

    Добавлена проверка горизонтальных и вертикальных линий

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ for (cell of cells) {
 function gameStep(event) {
     if (!event.target.innerHTML) {
         event.target.innerHTML = player;
-        gameFieldAddValue([event.target.dataset.value])
+        gameFieldAddValue([event.target.dataset.value]);
+        if (checkGame()) {
+            console.log('win: ', player)
+            return
+        }
         changePlayer();
     }
 }
@@ -27,5 +31,33 @@ function changePlayer() {
 }
 
 function gameFieldAddValue(value) {
-    gameField[Math.floor(value / 3)][value % 3] = player
+    gameField[Math.floor(value / gameField.length)][value % gameField.length] = player
+}
+
+function checkGame() {
+    return checkDiagonal() || checkLines();
+}
+
+function checkDiagonal() {
+    let rightDiagonal = true;
+    let leftDiagonal = true;
+    for (let i = 0; i < gameField.length; ++i) {
+        rightDiagonal = rightDiagonal && gameField[i][i] === player;
+        leftDiagonal = leftDiagonal && gameField[gameField.length - i - 1][i] === player;
+    } 
+    return rightDiagonal || leftDiagonal;
+}
+
+function checkLines() {
+    for (let x = 0; x < gameField.length; ++x) {
+        let vertical = true;
+        let horizontal = true;
+        for (let y = 0; y < gameField.length; ++y) {
+            vertical = vertical && gameField[y][x] === player;
+            horizontal = horizontal && gameField[x][y] === player;
+        }
+        if (vertical || horizontal) {
+            return true;
+        }
+    }
 }
